88bae65119fc99ad5e3b4d3187668d40a09237f2
Hyunsoo KIM <howeve18@gmail.com>
2026-02-09 10:02:57 +0900

    Feat : 로그아웃 상태 시 스터디 화면 접근 불가

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import Home from './pages/Home/Home';
 import StudyMain from './pages/StudyMain/StudyMain';
 import Study from './pages/Study/Study';
 import LoginPage from './pages/Login/Login';
+import ProtectedRoute from './routes/ProtectedRoute';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/study" element={<Study />} />
-      <Route path="/study-main" element={<StudyMain />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/study" element={<Study />} />
+        <Route path="/study-main" element={<StudyMain />} />
+      </Route>
     </Routes>
   );
 }
